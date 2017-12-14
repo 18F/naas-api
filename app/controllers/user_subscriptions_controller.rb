ba59cdc840d489fbe1f951dc0847b2298cb601dc
Jonathan Carmack <jonathan.carmack@gsa.gov>
2017-12-14 11:32:37 -0500

    All endpoints no working.  Might have to revisit the user_subscriptions controller, not sure having two routes going to one method is a good idea. Might make sense to at least limit posts from only one endpoint

--- app/controllers/user_subscriptions_controller.rb
+++ app/controllers/user_subscriptions_controller.rb
@@ -1,15 +1,23 @@
 # app/controllers/user_subscriptions_controller.rb
 class UserSubscriptionsController < ApplicationController
   before_action :set_notification
+  before_action :set_user
   before_action :set_notification_user_subscription, only: [:show, :update, :destroy]
 
   # GET /notifications/:notification_id/user_subscriptions
+  # GET /users/:user_id/user_subscriptions
   def index
-    #if params[]
-    json_response(@notification.user_subscriptions)
+
+    if @notification
+        json_response(@notification.user_subscriptions)
+    end
+    if request.path_parameters.has_key?(:user_id)
+        json_response(@user.user_subscriptions)
+    end
   end
 
   # GET /notifications/:notification_id/user_subscriptions/:id
+  # GET /users/:user_id/user_subscriptions/:id
   def show
     json_response(@user_subscription)
   end
@@ -17,7 +25,8 @@ class UserSubscriptionsController < ApplicationController
   # POST /notifications/:notification_id/user_subscriptions
   def create
     @notification.user_subscriptions.create!(user_subscription_params)
-    #@user.notifications.create!(notification_params)
+    @user = User.find(params[:user_id])
+    @user.user_subscriptions.create!(user_subscription_params)
     json_response(@user_subscription, :created)
   end
 
@@ -29,25 +38,31 @@ class UserSubscriptionsController < ApplicationController
 
   # DELETE /notifications/:notification_id/user_subscriptions/:id
   def destroy
-    @notification.destroy
+    @user_subscription.destroy
     head :no_content
   end
 
   private
 
   def user_subscription_params
-    params.permit(:name, :user_id)
+    params.permit(:name, :user_id, :notification_id)
   end
 
   def notification_params
-    params.permit(:name, :notification_id)
+    params.permit(:name, :notification_id, :user_id)
   end
 
   def set_notification
-    @notification = Notification.find(params[:notification_id])
+    if request.path_parameters.has_key?(:notification_id)
+      @notification = Notification.find(params[:notification_id])
+    end
   end
 
-
+  def set_user
+    if request.path_parameters.has_key?(:user_id)
+      @user = User.find(params[:user_id])
+    end
+  end
 
   def set_notification_user_subscription
     @user_subscription = @notification.user_subscriptions.find_by!(id: params[:id]) if @notification
