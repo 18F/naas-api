44ea7cf3965e45e0e7baa79cd70d071fe5d172ca
Jonathan Carmack <jonathan.carmack@gsa.gov>
2017-12-27 14:00:41 -0500

    Enabled cors

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,14 @@ module NaasApi
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Enable CORS
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
