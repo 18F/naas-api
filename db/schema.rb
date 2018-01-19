46ad59a5d4b32c428d61a1d33c8edf0966d9e8c9
Jonathan Carmack <jonathan.carmack@gsa.gov>
2018-01-18 21:34:16 -0500

    Migration ordering fixed, I was destroyed by the Loch ActiveRecord monster; some namespace fixes added, maybe a Rails 5 bug? (according to SO blog), seeds for user added, will need to change password before sharing this or making public

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180116153511) do
+ActiveRecord::Schema.define(version: 20180119012017) do
+
+  create_table "notification_events", force: :cascade do |t|
+    t.text "body"
+    t.boolean "unread"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_notification_events_on_user_id"
+  end
 
   create_table "notifications", force: :cascade do |t|
     t.string "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,11 @@
     a.created_by = 'Auto-generated'
   end
 end
+%w[jonathan.carmack@gsa.gov].each do |user|
+  User.find_or_create_by(email: user) do |u|
+    u.password = 'password12'
+    u.password_confirmation = 'password12'
+    u.last_name = 'Carmack'
+    u.phone = '18082246036'
+  end
+end
